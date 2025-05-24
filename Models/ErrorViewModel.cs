8e007e37ad948d0263061f030f7e0860bb1b70ee
VinhNguyenPhan <nguyenphanvinh2002@gmail.com>
2025-05-24 23:37:58 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NguyenPhanVinh_2021050911_KiemTra.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
